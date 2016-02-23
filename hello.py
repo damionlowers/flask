0636d93dce68cdfb127cb46c3fcb67adf173cd88
damionlowers <damion.lowers@gmail.com>
2016-02-23 00:18:23 -0500

    template added, forms added config added csef added

--- hello.py
+++ hello.py
@@ -1,19 +1,49 @@
 from flask import Flask, render_template
 from flask.ext.script import Manager
-from flask_bootstrap import Bootstrap
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
 
 app = Flask(__name__)
-bootstrap = Bootstrap()
-bootstrap.init_app(app)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
 manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
-@app.route('/')
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+# @app.route('/')
+# def index():
+# 	return render_template('index.html')
+
+# @app.route('/user/<name>')
+# def user(name):
+# 	return render_template('user.html',name=name)
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html')
-
-@app.route('/user/<name>')
-def user(name):
-	return render_template('user.html',name=name)
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 
 if __name__ == '__main__':
